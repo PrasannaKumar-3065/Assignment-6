efabd9013bfde7435d163b32d99da3e2cbdfd34f
prasanna-761 <123439582+prasanna-761@users.noreply.github.com>
2023-01-27 15:43:00 +0530

    Update App.java

--- src/main/java/pk/App.java
+++ src/main/java/pk/App.java
@@ -1,22 +1,6 @@
 package pk;
 import java.util.*;
 import java.util.logging.*;
-
-/*"1. Design a class for a simple database connection pool. The class should have the following features:
-    - A private constructor to prevent the class from being instantiated directly.
-    - A static method that returns an instance of the class (following the singleton pattern).
-    - A method that returns a new connection to the database.
-    - A method that closes all open connections and releases them back to the pool.
-2. Design a class for a basic point in 2D space. The class should have the following features:
-    - Instance variables for the x and y coordinates of the point.
-    - A constructor that allows the user to create a point with given x and y coordinates.
-    - An equals method that returns true if the given object is a point with the same x and y coordinates, and false otherwise.
-    - A clone method that creates and returns a new point with the same x and y coordinates as the original point.
-3. Design a class for a basic credit card. The class should have the following features:
-    - Instance variables for the cardholder's name, card number, and expiration date.
-    - A constructor that allows the user to create a credit card with a given name, card number, and expiration date.
-    - An equals method that returns true if the given object is a credit card with the same card number, and false otherwise.
-    - A clone method that creates and returns a new credit card with the same name, card number, and expiration date as the original credit card."*/
 class CREDIT implements Cloneable{
     private static final Logger LOGGER =  Logger.getLogger("InfoLogging");
     String holdername;
